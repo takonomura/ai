21e8e565a09bbc2dacd49f9e493803a182e955b2
takonomura <takonomura@users.noreply.github.com>
2025-03-09 21:29:35 +0900

    Use apiUrl for reversi

--- src/modules/reversi/back.ts
+++ src/modules/reversi/back.ts
@@ -452,7 +452,7 @@ class Session {
 			}
 
 			try {
-				const res = await got.post(`${config.host}/api/notes/create`, {
+				const res = await got.post(`${config.apiUrl}/notes/create`, {
 					json: body
 					headers: config.headers,
 				}).json();
